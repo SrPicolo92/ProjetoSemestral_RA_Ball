b63ce43f03a176e90a5148bb4a46148bbb1bbd6d
Rafael Picolo <rpicolo26@gmail.com>
2019-06-21 10:41:48 -0300

    Menu e Trimmer prontos

---
+++ Ball_RA/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController: MonoBehaviour
+{
+    public Animator animator;
+
+
+    public void Avancar()
+    {
+        animator.SetTrigger("Avancar");
+        Debug.Log("Avançando!");
+    }
+
+    public void Retornar()
+    {
+        animator.SetTrigger("Retornar");
+        Debug.Log("Retornando");
+    }
+}
---
+++ Ball_RA/Assets/Scripts/Manager_Scenes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager_Scenes : MonoBehaviour
+{
+    public void changeToExplosao()
+    {
+        SceneManager.LoadScene("Trimmer");
+    }
+
+    public void changeToConsersao()
+    {
+        SceneManager.LoadScene("Conversao");
+    }
+
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+}
